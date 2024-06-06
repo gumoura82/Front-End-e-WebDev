0543f889d582326245cfef6b5fbe315981334607
Gustavo Moura <gumoura82@gmail.com>
2024-06-06 15:17:42 -0300

    Sessão "Entre em Contato" atualizada

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+function enviarDados() {
+    const nomeInput = document.getElementById('nome');
+    const emailInput = document.getElementById('email');
+
+    nomeInput.value = '';
+    emailInput.value = '';
+
+    alert("Enviado com sucesso!")
+}
